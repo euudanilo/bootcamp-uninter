ebdb558db320cbf8053e64b7039664257c62cad4
Danilo <daniloalvesdafonseca@gmail.com>
2026-04-15 17:52:41 -0300

    chore: inicializa projeto Spring Boot

---
+++ src/main/java/com/bootcamp_uninter/BootcampUninterApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp_uninter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampUninterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampUninterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp_uninter/BootcampUninterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp_uninter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampUninterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
